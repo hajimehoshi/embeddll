8ab7efa2394de1797d663d39d88bb67566ddf167
Hajime Hoshi <hajimehoshi@gmail.com>
2018-12-28 18:14:13 +0900

    Add 32bit version (not tested yet)

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 //go:generate x86_64-w64-mingw32-gcc -c -o hi.o hi.c
 //go:generate x86_64-w64-mingw32-gcc -shared -o hi.dll hi.o
+//go:generate i686-w64-mingw32-gcc -c -o hi32.o hi.c
+//go:generate i686-w64-mingw32-gcc -shared -o hi32.dll hi32.o
 //go:generate file2byteslice -input hi.dll -output hidll.go -package main -var hiDLL
+//go:generate file2byteslice -input hi32.dll -output hi32dll.go -package main -var hi32DLL
 //go:generate gofmt -s -w .
-//go:generate rm hi.dll hi.o
+//go:generate rm hi.dll hi32.dll hi.o hi32.o
 
 package main
 
@@ -10,6 +13,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"runtime"
 
 	"golang.org/x/sys/windows"
 )
@@ -24,7 +28,11 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	if err := ioutil.WriteFile(filepath.Join(dir, "hi.dll"), hiDLL, 0777); err != nil {
+	dll := hiDLL
+	if runtime.GOARCH == "386" {
+		dll = hi32DLL
+	}
+	if err := ioutil.WriteFile(filepath.Join(dir, "hi.dll"), dll, 0777); err != nil {
 		panic(err)
 	}
 
