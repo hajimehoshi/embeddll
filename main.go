882b70a8951e15716acdb2afb223a61c582b6a43
Hajime Hoshi <hajimehoshi@gmail.com>
2018-12-28 20:38:49 +0900

    Deterministic build; Fix filenames

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
-//go:generate x86_64-w64-mingw32-gcc -c -o hi.o hi.c
-//go:generate x86_64-w64-mingw32-gcc -shared -o hi.dll hi.o
-//go:generate i686-w64-mingw32-gcc -c -o hi32.o hi.c
-//go:generate i686-w64-mingw32-gcc -shared -o hi32.dll hi32.o
-//go:generate file2byteslice -input hi.dll -output hidll.go -package main -var hiDLL
-//go:generate file2byteslice -input hi32.dll -output hi32dll.go -package main -var hi32DLL
+//go:generate x86_64-w64-mingw32-gcc -c -o hi_amd64.o hi.c
+//go:generate x86_64-w64-mingw32-gcc -shared -o hi_amd64.dll -Wl,--no-insert-timestamp hi_amd64.o
+//go:generate i686-w64-mingw32-gcc -c -o hi_386.o hi.c
+//go:generate i686-w64-mingw32-gcc -shared -o hi_386.dll -Wl,--no-insert-timestamp hi_386.o
+//go:generate file2byteslice -input hi_amd64.dll -output hidll_amd64.go -package main -var hiDLL
+//go:generate file2byteslice -input hi_386.dll -output hidll_386.go -package main -var hiDLL
 //go:generate gofmt -s -w .
-//go:generate rm hi.dll hi32.dll hi.o hi32.o
+//go:generate rm hi_amd64.dll hi_386.dll hi_amd64.o hi_386.o
 
 package main
 
@@ -24,15 +24,11 @@ var (
 )
 
 func init() {
-	dir, err := ioutil.TempDir("", "windlltest")
+	dir, err := ioutil.TempDir("", "embeddll")
 	if err != nil {
 		panic(err)
 	}
-	dll := hiDLL
-	if runtime.GOARCH == "386" {
-		dll = hi32DLL
-	}
-	if err := ioutil.WriteFile(filepath.Join(dir, "hi.dll"), dll, 0777); err != nil {
+	if err := ioutil.WriteFile(filepath.Join(dir, "hi.dll"), hiDLL, 0777); err != nil {
 		panic(err)
 	}
 
